6e35f59fb713d3d22b665bba1826c51b3cbcfee0
Matthew Nusbaum <m.guy008@gmail.com>
2024-12-11 20:01:48 -0800

    Updated playermanagementservice

--- TeamManagementService (1)/PlayerManagementService/Migrations/20241212033727_UpdateDatabase.Designer.cs
+++ TeamManagementService (1)/PlayerManagementService/Migrations/20241212033727_UpdateDatabase.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PlayerManagementService.Data;
 
@@ -10,9 +11,11 @@ using PlayerManagementService.Data;
 namespace PlayerManagementService.Migrations
 {
     [DbContext(typeof(PlayerContext))]
-    partial class PlayerContextModelSnapshot : ModelSnapshot
+    [Migration("20241212033727_UpdateDatabase")]
+    partial class UpdateDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TeamManagementService (1)/PlayerManagementService/Migrations/20241212033727_UpdateDatabase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlayerManagementService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PlayerId",
+                table: "Players",
+                newName: "playerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "playerId",
+                table: "Players",
+                newName: "PlayerId");
+        }
+    }
+}
